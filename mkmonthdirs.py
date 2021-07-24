3b30e3621f2c3a4718c53abe76d5704a406ee5c3
anonymous <qhiGenrRvta2@users.noreply.github.com>
2021-07-24 16:42:31 +0100

    First version.

---
+++ mkmonthdirs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import calendar
+
+def main():
+    """
+    Creates January, February ... December directories.
+    """
+    months = [calendar.month_name[i] for i in range(1, 13)]
+
+    pwd = os.getcwd()
+        
+    for month in months:
+        month_dir = os.path.isdir(os.path.join(pwd, month))
+        if not month_dir: 
+            os.mkdir(dir_name)
+
+if __name__=="__main__":
+    main()
