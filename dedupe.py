f997afe889202cec123f12c26a89093a60edf41b
anonymous <qhiGenrRvta2@users.noreply.github.com>
2021-09-26 10:56:57 +0100

    corrections

--- dedupe.py
+++ dedupe.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-import os
 import hashlib
+import os
+import sys
+
 import rich.console
 
 def main():
@@ -9,11 +11,21 @@ def main():
     Handles the user interface.
     Main application logic is in the find_duplicates function.
     """
-    # TODO: accept command line argument specifying a path to look in. 
-    start_directory = os.getcwd()
-    
+
+    # Handle command line arguments
+    if len(sys.argv) == 2:
+        try:
+            start_directory = check_input(sys.argv[1])
+        except ValueError:
+            sys.exit(f"Could not find {sys.argv[1]}")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many arguments. \n Usage: dedupe.py DIRECTORY")
+    else:
+        start_directory = os.getcwd()
+
+    print(start_directory)
     # Set up a console for showing progress bar / providing nice output
-    console = rich.console.Console(force_terminal=False)
+    console = rich.console.Console(highlight=False)
 
     # Do the useful stuff.
     with console.status(status="Searching for duplicate files"):
@@ -22,7 +34,19 @@ def main():
     # Provide output.
     if result == 0:
         console.print("No duplicates found!")
+
+def check_input(argument):
+    """
+    checks if argument is a valid relative or absolute path
+    """
+    if os.path.isdir(argument):
+        return argument
+    elif os.path.isdir(os.path.join(os.getcwd(), argument)):
+        return os.path.join(os.getcwd(), argument)
     
+    # If we got here, value is bad.
+    raise ValueError("invalid path specified")
+
 
 def find_duplicates(start_directory, output_console):
     """
@@ -33,7 +57,8 @@ def find_duplicates(start_directory, output_console):
     """
 
     files = get_files(start_directory)
-    hash_table = {files[entry]['hash']: [] for entry in files}
+    # 'None' means we could not open the file due to a permissions error.
+    hash_table = {files[entry]['hash']: [] for entry in files if files[entry]['hash'] is not None}
 
     for current_file in files:
         hash_table[files[current_file]['hash']].append(files[current_file]['location'])
@@ -52,27 +77,38 @@ def generate_hash(my_file):
     """
     Generates the md5 hash of my_file.
     """
-    with open(my_file, 'rb') as current_file:
-        buffered_file = current_file.read()
-        hasher = hashlib.md5()
-        hasher.update(buffered_file)
+    try:
+        with open(my_file, 'rb') as current_file:
+            buffered_file = current_file.read()
+            hasher = hashlib.md5()
+            hasher.update(buffered_file)
     
-    return hasher.hexdigest()
-
+        return hasher.hexdigest()
+    except PermissionError:
+        return None
+    except FileNotFoundError:
+        print(f"{my_file=} does not exist")
+        return None
 
 def get_files(start_directory):
     """
     Finds files in a directory (and any sub-directories) by depth-first search.
     """
-    results = {entry: {'location': os.path.join(start_directory, entry), 'hash': generate_hash(entry)} for entry in os.listdir(start_directory) if not os.path.isdir(entry)} 
+    results = {entry: {'location': os.path.join(start_directory, entry), 'hash': generate_hash(os.path.join(start_directory, entry))} for entry in os.listdir(start_directory) if not os.path.isdir(os.path.join(start_directory, entry))} 
     # Also need to look in subdirectories. 
-    frontier = [os.path.join(start_directory, entry) for entry in os.listdir(start_directory) if os.path.isdir(entry)]
+    frontier = [os.path.join(start_directory, entry) for entry in os.listdir(start_directory) if os.path.isdir(os.path.join(start_directory, entry))]
     
     while(frontier):
         # Take a subdirectory.
         current_directory = frontier.pop()
-        # Add any child directories to the frontier.
-        frontier += [os.path.join(current_directory, entry) for entry in os.listdir(current_directory) if os.path.isdir(entry)]
+        
+        # Add any child directories to the frontier. 
+        # Try/except block is needed to handle permission errors. Cannot even list the contents of ~/.Trash, apparently.
+        try:
+            frontier += [os.path.join(current_directory, entry) for entry in os.listdir(current_directory) if os.path.isdir(os.path.join(current_directory, entry))]
+        except PermissionError:
+            continue
+
         # Add any new files to our dictionary.
         new_files = [os.path.join(current_directory, entry) for entry in os.listdir(current_directory) if not os.path.isdir(os.path.join(current_directory, entry))]
         for entry in new_files:
